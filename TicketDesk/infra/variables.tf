47f33a947b57c98d03826daa0ac586fc0df0d1bf
Chakradhar <135502086+Mr-Chakradhar09@users.noreply.github.com>
2026-08-18 13:10:07 +0530

    Change default project name to 'ticketdesk-v1'

--- TicketDesk/infra/variables.tf
+++ TicketDesk/infra/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "ticketdesk-v2"
+  default     = "ticketdesk-v1"
 }
 
 variable "app_port" {
